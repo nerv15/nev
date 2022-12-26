fd7ff5264d78bac511d2bb5f5deb60d9d166dc8a
Nerv <15@gmail.com>
2022-12-26 16:17:31 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,126 @@
+ var mark0 = +prompt('Введите свой возраст в цифрах');
+
+ var mark0 = +prompt('Вы еще молоды Вам нужно учится');
+
+ var mark0 = +prompt('Вам нужно работать');
+
+ var mark0 = +prompt('Вам скоро на пенсию');
+
+ var mark0 = +prompt('Вы пенсионер');
+
+ var mark0 = +prompt('Что-то пошло не так');
+
+
+
+ if(0 < 18 && !isNaN(0) && !isNaN(18) ){
+   
+     console.log('Вы еще молоды Вам нужно учится');
+ } else if(18 > 50 && !isNaN(18) && !isNaN(50)){
+        
+     console.log('Вам нужно работать');
+ } else if (50 > 59 && !isNaN(50) && !isNaN(59)){
+   
+         console.log('Вам скоро на пенсию');
+ } else if(59 > 100 && !isNaN(59) && !isNaN(100)){
+    
+     console.log('Вы пенсионер');
+ } else{
+     console.log('Что-то пошло не так');
+ }
+   
+
+
+// var time = +prompt('Введите время в цифрах')
+  
+
+ switch (time) {
+     case 1:
+         alert('1 час ночи')
+         break;
+     case 2:
+         alert('2 часа ночи')
+         break;
+     case 3:
+         alert('3 часа ночи')
+         break;
+     case 4:
+         alert('4 часа ночи')
+         break;
+     case 5:
+         alert('5 часов ночи')
+         break;
+     case 6:
+         alert('6 часов ночи')
+         break;
+     case 7:
+         alert('7 часов утра')
+         break;
+     case 8:
+         alert('8 часов утра')
+         break;
+     case 9:
+         alert('9 часов утра')
+         break;
+     case 10:
+         alert('10 часов утра')
+         break;
+     case 11:
+         alert('11 часов дня')
+         break;
+     case 12:
+         alert('12 часов дня')
+         break;
+     case 13:
+         alert('13 часов дня')
+         break;
+     case 14:
+         alert('14 часов дня')
+         break;
+     case 15:
+         alert('15 часов дня')
+         break;
+     case 16:
+         alert('16 часов дня')
+         break;
+     case 17:
+         alert('17 часов дня')
+         break;
+     case 18:
+         alert('18 часов вечера')
+         break;
+     case 19:
+         alert('19 часов вечера')
+         break;
+     case 20:
+         alert('20 часов вечера')
+         break;
+     case 21:
+         alert('21 часов вечера')
+         break;
+     case 22:
+        alert('22 часов вечера')
+         break;
+     case 23:
+         alert('23 часов вечера')
+         break;
+ }
+
+
+
+  var mun1 = +prompt('Введите первое число');
+  var mun2 = +prompt('Введите второе число');
+  var mun3 = +prompt('Введите третье число');
+
+
+  if(mun1 < mun2 &&  mun3 > mun2 ){
+     alert('Среднее число' + mun2 )
+  }else if(mun2 < mun1 &&  mun3 > mun1){
+    alert('Среднее число' + mun1 )
+  }else if(mun1 < mun3 &&  mun2 > mun3){
+    alert('Среднее число' +mun3 )
+  }
+
+
+
+
+
